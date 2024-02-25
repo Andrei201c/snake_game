d3156a5595c89fe5caba8a4468b72db9e0c91a89
Andrei201c <andreiciuvat@gmail.com>
2024-02-25 14:33:31 +0200

    Add code

---
+++ main.py
@@ -0,0 +1,50 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Score
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake")
+screen.tracer(0)
+
+
+snake = Snake()
+food = Food()
+score = Score()
+
+keep = True
+
+screen.listen()
+screen.onkey(snake.go_left, "a")
+screen.onkey(snake.go_right, "d")
+screen.onkey(snake.go_up, "w")
+screen.onkey(snake.go_down, "s")
+
+while keep:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move_snake()
+
+    # Detect collision with the food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase()
+
+    # Detect collision with the walls
+    if snake.head.xcor() > 288 or snake.head.xcor() < -288 or snake.head.ycor() > 288 or snake.head.ycor() < -288:
+        score.game_over()
+        keep = False
+
+    # Detect the collision with body
+    for segment in snake.segments[1:]:  # This part of code segments[1:] e de la list and slices ->
+        if snake.head.distance(segment) < 10:          # si incepe sa verifice de na indexul unu sarind peste index 0 ->
+            keep = False                                   # poti sa adaugi si punct final si peste scat sa sara [2:3:1]
+            score.game_over()
+
+
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,54 @@
+from turtle import Turtle
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.draw_snake()
+        self.head = self.segments[0]
+
+    def draw_snake(self):
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle()
+        new_segment.shape("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.speed('fast')
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move_snake(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            cor_x = self.segments[seg_num - 1].xcor()
+            cor_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(cor_x, cor_y)
+        self.head.forward(20)
+
+    def go_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def go_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def go_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def go_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
